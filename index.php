88c3c9debd84f4111e4054801636efa9d56ec126
Pennii <matiasspennino@gmail.com>
2025-01-03 18:35:21 +0100

    pagina de inicio y login

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Say n Fit</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <img src="imagenes/logo.png" alt="" id="logo">
+    </header>
+    <section>
+        <div id="presentacion">
+            <p>¿Quieres convertirte en un poderoso guerrero? ¿Estás cansado de no saber cómo dar el 100% en tus entrenamientos? En Say n Fit te podemos ayudar.<br>
+                En nuestra aplicación podrás conectar con personas que buscan lo mismo que tú: aprender y entrenar juntos con el objetivo de volverse más fuertes.<br>
+                ¿Te ves capaz de ayudar a los demás o de organizar competencias? Crea un grupo para reunir a la gente.<br>
+                ¿Qué estás esperando? ¡Únete al mundo del fitness de una manera única hoy mismo!
+            </p>
+        </div>
+        <div id="login">
+            <form action="login.php" method="post">
+                <label for="nombre">Nombre de usuario:</label><br>
+                <input type="text" id="nombre" name="nombre">
+                <br><br>
+                <label for="clave">Contraseña:</label><br>
+                <input type="text" id="clave" name="clave">
+                <br><br>
+                <button value="Ingresar" name="ingresar">Ingresar</button>
+                <button value="Registrarse" name="registrarse">Registrarse</button>
+            </form>
+        </div>
+    </section>
+</body>
+
+</html>
